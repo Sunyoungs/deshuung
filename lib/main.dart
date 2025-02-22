982d1302b734189c192c96f26cece2f1d4d39062
Sunyoungs <lucy4633@naver.com>
2025-02-23 02:25:41 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,48 +14,25 @@ class MyApp extends StatelessWidget {
       title: 'Deshuung',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Deshuung')),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: SearchBar(
-            trailing: [Icon(Icons.search)],
-            hintText: "검색어를 입력하세요",
-          ),
+        brightness: Brightness.light, 
+        primaryColor: Colors.white, 
+        scaffoldBackgroundColor: Colors.white, 
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white, 
+          selectedItemColor: Colors.black, 
+          unselectedItemColor: Colors.grey,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+          bodyMedium: TextStyle(color: Colors.black),
+          titleLarge: TextStyle(color: Colors.black),
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Deshunng"),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchScreen()),);
-            },
-          ),
-        ],
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const Text("pay"),
-          const Text("my favorite"),
-          const Text("Home"),
-          const Text("my car"),
-          const Text("my page"),
-        ],
-      ),
+      home: const TabView(),
     );
   }
 }
@@ -72,6 +49,64 @@ class SearchScreen extends StatelessWidget {
       body: const Center(
         child: Text("This is the Search Screen."),
       ),
+    );
+  }
+}
+
+class TabView extends StatefulWidget {
+  const TabView({super.key});
+  @override
+  State<TabView> createState() => _TabViewState();
+}
+
+class _TabViewState extends State<TabView> with SingleTickerProviderStateMixin {
+  int _index = 0;
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+    _tabController.addListener(() {setState(() {_index = _tabController.index;});});
+  }
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Deshuung')),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SearchBar(
+          trailing: const [Icon(Icons.search)], hintText: "검색어를 입력하세요",
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          _tabController.animateTo(index);
+        },
+        currentIndex: _index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.payments), label: 'pay'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'my favorite'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.directions_car), label: 'my car'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'my page'),
+        ],
+      ),
+      /*child: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _tabController,
+        children: const [
+          PayPage(),
+          FavoritePage(),
+          HomePage(),
+          CarPage(),
+          PagePage(),
+        ],
+      ),*/
     );
   }
 }
