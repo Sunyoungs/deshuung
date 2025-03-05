0dfcafe520b2d6d424c616de9a5c06483c104f6c
6kitty <sixeunseoth@gmail.com>
2025-03-06 01:02:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors=require('cors');
+const axios=require('axios');
+const { admin, db } = require("./config/firebaseAdmin");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var accountsRouter=require('./routes/accounts');
+var carsRouter=require('./routes/cars');
+var paymentsRouter=require('./routes/payments');
+var storesRouter=require('./routes/stores');
+var driveRouter=require('./routes/drive');
 
 var app = express();
-app.use(cors());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +29,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/accounts', accountsRouter);
+app.use('/cars',carsRouter);
+app.use('/payments',paymentsRouter);
+app.use('/stores',storesRouter);
+app.use('/drive',driveRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +49,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// 테스트 라우트 (Firestore에서 데이터 조회)
+app.get("/", async (req, res) => {
+  const snapshot = await db.collection("accounts").get();
+  const users = snapshot.docs.map(doc => doc.data());
+  res.json({ users });
 });
 
 module.exports = app;
